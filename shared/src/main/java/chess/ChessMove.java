583416ef7cd2ed7dc52443c2dd8e9eae33f7c77c
Kolton Baldwin <koltonrb@byu.edu>
2025-09-10 19:56:38 -0600

    beginning updates to ChessBoard, ChessMove, ChessPiece, and ChessPosition to debug BishopMoveTests.bishopMoveUntilEdge().

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,31 +8,38 @@ package chess;
  */
 public class ChessMove {
 
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
-     * Gets the type of piece to promote a pawn to if pawn promotion is part of this
+     * Gets the type of piece to which to promote a pawn to if pawn promotion is part of this
      * chess move
      *
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
     }
 }
