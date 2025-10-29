fcb9485d9e1dfd2816832a5304077d5e7c3eb022
Kolton Baldwin <koltonrb@byu.edu>
2025-10-28 23:37:21 -0600

    added createStatements

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -1,15 +1,71 @@
 package dataaccess;
 
 import exception.DataAccessException;
+import exception.ResponseException;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
 import requests.ClearRequest;
 
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MySqlDataAccess implements DataAccess {
+
+    public void MySqlDataAccess() throws ResponseException {
+        configureDatabase();
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                `user_id` int NOT NULL AUTO_INCREMENT,
+                `username` varchar(256) NOT NULL,
+                `password` varchar(256) NOT NULL,
+                'email' varchar(256) NOT NULL,
+                PRIMARY KEY ('user_id')
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS authorizations (
+                `authToken` varchar(256) NOT NULL,
+                `username` varchar(256)
+            PRIMARY KEY (authToken),
+            INDEX (username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                `game_id` int NOT NULL AUTO_INCREMENT,
+                `whiteUsername` varchar(256),
+                `blackUsername` varchar(256),
+                `gameName` varchar(256) NOT NULL,
+                `game` varchar(10000),
+            PRIMARY KEY ('game_id'),
+            INDEX(gameName)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+    //FIXME: is `game` varchar(10000) enough characters to serialize a full chessboard? it is way too much memory?
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()){
+            for (String statement : createStatements){
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
     @Override
     public HashMap<String, UserData> getUsers() {
         return null;
