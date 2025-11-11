15cebb09fd4c087162996b6f500da75021496ba7
Kolton Baldwin <koltonrb@byu.edu>
2025-11-10 21:58:38 -0700

    just saving...

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import requests.RegisterRequest;
 import results.RegisterResult;
 
+import java.net.ResponseCache;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -42,9 +43,27 @@ public class ServerFacade {
         try {
             return client.send(request, BodyHandlers.ofString());
         } catch (Exception ex) {
+            // todo: what sorts of things will cause an error here?
             throw new ResponseException( ResponseException.Code.ServerError, ex.getMessage());
         }
     }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass){
+        var status = response.statusCode();
+        if (!isSuccessful(status)){
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {return status / 100 == 2; }
 
     public RegisterResult registerUser(RegisterRequest request){
 
