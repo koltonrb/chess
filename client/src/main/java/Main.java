c459257a23f6bddd50f1423f258738d407c24ff4
Kolton Baldwin <koltonrb@byu.edu>
2025-11-10 20:49:48 -0700

    adds ServerFacade and modifies client.main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,7 +2,18 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+        try {
+            new ChessClient(serverUrl).run();
+        } catch (Throwable ex){
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
+
     }
 }
