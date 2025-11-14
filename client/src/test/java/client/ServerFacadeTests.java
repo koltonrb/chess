6d24a1f39be46addd9b42e7545901c271acb44e7
Kolton Baldwin <koltonrb@byu.edu>
2025-11-14 07:42:11 -0700

    more tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,11 +1,15 @@
 package client;
 
 import exception.ResponseException;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import requests.*;
+import results.JoinGameResult;
 import results.LoginResult;
 import results.LogoutResult;
 import server.Server;
+
+import java.util.ArrayList;
 
 
 public class ServerFacadeTests {
@@ -108,5 +112,45 @@ public class ServerFacadeTests {
                 ()->facade.createGame(new CreateGameRequest("game1")), "authToken needed to create a game");
     }
 
+    @Test
+    @DisplayName("positiveCreateGame")
+    void positiveListGame() throws ResponseException {
+        var authData = facade.registerUser(new RegisterRequest("player1", "password", "p1@email.com"));
+        facade.setAuthToken(authData.authToken());
+        var loginResult = facade.loginUser(new LoginRequest("player1", "password"));
+        var listGameResult = facade.listGames( new ListGamesRequest() );
+        Assertions.assertEquals(new ArrayList<GameData>(), listGameResult.games(), "list should be empty when no games created");
+    }
+
+    @Test
+    @DisplayName("negativeCreateGame")
+    void negativeListGames() throws ResponseException{
+        var authData = facade.registerUser(new RegisterRequest("player1", "password", "p1@email.com"));
+        ResponseException myException = Assertions.assertThrows(ResponseException.class,
+                ()-> facade.listGames( new ListGamesRequest()), "should only be able to list games if logged in and authorized");
+
+    }
+
+    @Test
+    @DisplayName("positiveJoinGame")
+    void positveJoinGame() throws ResponseException{
+        var authData = facade.registerUser(new RegisterRequest("player1", "password", "p1@email.com"));
+        facade.setAuthToken(authData.authToken());
+        var createGameResult = facade.createGame(new CreateGameRequest("game1"));
+        var joinGameResult = facade.joinGame(new JoinGameRequest("WHITE", 1));
+        Assertions.assertEquals(new JoinGameResult(), joinGameResult, "problem joining game");
+    }
+
+    @Test
+    @DisplayName("negativeJoinGame")
+    void negativeJoinGame() throws ResponseException{
+        var authData = facade.registerUser(new RegisterRequest("player1", "password", "p1@email.com"));
+        facade.setAuthToken(authData.authToken());
+        var createGameResult = facade.createGame(new CreateGameRequest("game1"));
+        ResponseException myException = Assertions.assertThrows(ResponseException.class,
+                () -> facade.joinGame(new JoinGameRequest("WHITE", 2)),
+                "you should throw an error when the user tries to join a game that does not exist");
+
+    }
 
 }
