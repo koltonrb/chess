0c96d758c4dc577951592e845dd1530df99f5a42
Kolton Baldwin <koltonrb@byu.edu>
2025-12-02 20:33:36 -0700

    flushes out ConnectionManager

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -1,5 +1,41 @@
 package server;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class ConnectionManager {
-    public final ConcurrentHashMap<Integer gameID, Arraylist>
+    private ConcurrentHashMap<Integer, CopyOnWriteArrayList<Session>> sessionsInGame = new ConcurrentHashMap<>();
+
+    public void saveSession(Integer gameID, Session session){
+        sessionsInGame.computeIfAbsent(gameID, theGameID -> new CopyOnWriteArrayList<>()).add(session);
+    }
+
+    public void removeSession(Integer gameID, Session session){
+        CopyOnWriteArrayList<Session> pointerToList = sessionsInGame.get(gameID);
+        if (pointerToList != null){
+            pointerToList.remove(session);
+
+            if (pointerToList.isEmpty()) {
+                sessionsInGame.remove(gameID);
+            }
+        }
+    }
+
+    public List<Session> getSessions(Integer gameID){
+        return sessionsInGame.getOrDefault(gameID, CopyOnWriteArrayList.of());
+    }
+
+    public void broadcast(Integer gameID, Session excludeSession, String msg) throws IOException {
+        for (Session c : getSessions(gameID)){
+            if (c.isOpen()) {
+                if (!c.equals(excludeSession)){
+                    c.getRemote().sendString(msg);
+                }
+            }
+        }
+    }
 }
--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,14 +1,22 @@
 package server;
 
 import com.google.gson.Gson;
+import dataaccess.DataAccess;
 import exception.UnauthorizedException;
 import io.javalin.websocket.*;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.commands.*;
 
+import java.io.IOException;
+
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final DataAccess dataAccess;
+
+    public WebSocketHandler (DataAccess dataAccess){
+        this.dataAccess = dataAccess;
+    }
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -24,26 +32,37 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         try {
             UserGameCommand command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
             gameID = command.getGameID();
-            String username = getUsername(command.getAuthString());
-            saveSession(gameID, session);
+            String username = dataAccess.getAuth(command.getAuthToken()).username();
+            connections.saveSession(gameID, session);
 
             switch (command.getCommandType()) {
                 case CONNECT -> connect(session, username, (ConnectCommand) command );
-                case MAKE_MOVE -> makeMove(session, username, (MakeMoveCommand) command);
-                case LEAVE -> leaveGame(session, username, (LeaveGameCommand) command);
-                case RESIGN -> resign(session, username, (ResignCommand) command);
+                //TODO
+//                case MAKE_MOVE -> makeMove(session, username, (MakeMoveCommand) command);
+//                case LEAVE -> leaveGame(session, username, (LeaveGameCommand) command);
+//                case RESIGN -> resign(session, username, (ResignCommand) command);
             }
         } catch (UnauthorizedException ex) {
-            sendMessage(session, gameID, new ErrorMessage("Error: unauthorized"));
+            sendMessage(session, "Error: unauthorized");
         } catch (Exception ex) {
             ex.printStackTrace();
-            sendMessage(session, gameID, new ErrorMessage("Error: " + ex.getMessage()));
+            sendMessage(session, "Error: " + ex.getMessage());
         }
     }
 
     @Override
     public void handleClose(WsCloseContext ctx) {
         System.out.println("Websocket closed");
+    }
+
+    public void sendMessage(Session session, String msg) {
+        if (session.isOpen()) {
+            try{
+                session.getRemote().sendString(msg);
+            } catch (IOException ex) {
+                System.out.println("Failed to send message to session; " + ex.getMessage());
+            }
+        }
     }
 
     private void connect(Session session, String username, ConnectCommand command) {
